0aebdbae78907a16d0a13c6d536e9d846b6282f7
wayne <lycazure@hotmail.com>
2025-05-19 23:20:49 -0500

    update the data strucutre

---
+++ Lizection/DataModels/ToGoItem.swift
@@ -0,0 +1,39 @@
+import Foundation
+import SwiftData
+import CoreLocation
+
+@Model
+class ToGoItem {
+    var id: UUID
+    var name: String
+    var address: String?
+    var latitude: Double
+    var longitude: Double
+    var startTime: Date
+    var endTime: Date
+    var eventIdentifier: String?
+
+    init(
+        id: UUID = UUID(),
+        name: String,
+        address: String? = nil,
+        latitude: Double,
+        longitude: Double,
+        startTime: Date,
+        endTime: Date,
+        eventIdentifier: String? = nil
+    ) {
+        self.id = id
+        self.name=name
+        self.address = address
+        self.latitude = latitude
+        self.longitude = longitude
+        self.startTime = startTime
+        self.endTime = endTime
+        self.eventIdentifier = eventIdentifier
+    }
+
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
